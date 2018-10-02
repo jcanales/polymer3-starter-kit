465f01234a4d5795c06bf8a0be863ea42925cea3
zeus <jonathan@zeus.local>
2018-10-02 09:43:07 -0500

    add new changes

--- public/src/my-view1.js
+++ public/src/my-view1.js
@@ -1,3 +1,5 @@
+
+
 /**
  * @license
  * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
@@ -17,8 +19,7 @@ import '@polymer/paper-item/paper-item.js';
 import '@polymer/neon-animation/neon-animation.js';
 import '@polymer/paper-listbox/paper-listbox.js';
 import '@polymer/iron-form/iron-form.js';
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from 'firebase/app'
 
 
 class MyView1 extends PolymerElement {
@@ -39,38 +40,28 @@ class MyView1 extends PolymerElement {
           <paper-input id="school_name" type="text" label="Nombre Escuela"></paper-input>
           <paper-input id="school_address" type="text" label="Dirección"></paper-input>
           <paper-input id="school_type" type="text" label="Tipo de Escuela"> </paper-input>
-
             <br><br>
             <br>
           <paper-button raised on-click="add">Submit</paper-button>
           <paper-button raised on-click="clear">Reset</paper-button>
       </form>
       </div>
-
-      <script src="node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
-      <script src="node_modules/web-animations-js/web-animations-next-lite.min.js"></script>
-
-          <script src="https://www.gstatic.com/firebasejs/5.5.2/firebase.js"></script>
-          <script src="https://www.gstatic.com/firebasejs/5.5.2/firebase-database.js"></script>
-          <script>
-         // Initialize Firebase
-            var config = {
-              apiKey: "AIzaSyC0Mt_4iqqTREXNK5yf6LSdjjJQ_N-Loac",
-              authDomain: "cfid-300bc.firebaseapp.com",
-              databaseURL: "https://cfid-300bc.firebaseio.com",
-              projectId: "cfid-300bc",
-              storageBucket: "cfid-300bc.appspot.com",
-              messagingSenderId: "1069062310022"
-            };
-            firebase.initializeApp(config);
-          </script>
-
+      <script>
+        // Initialize Firebase
+        var config = {
+          apiKey: "AIzaSyC0Mt_4iqqTREXNK5yf6LSdjjJQ_N-Loac",
+          authDomain: "cfid-300bc.firebaseapp.com",
+          databaseURL: "https://cfid-300bc.firebaseio.com",
+          projectId: "cfid-300bc",
+          storageBucket: "cfid-300bc.appspot.com",
+          messagingSenderId: "1069062310022"
+        };
+      </script>
     `;
   }
               add() {
-                      var database = firebase.database();
-                          function writeUserData(school_name, school_address, school_type) {
-                          firebase.database().ref('schools/').set({
+                        function writeUserData(school_name, school_address, school_type) {
+                        firebase.database().ref('schools/').set({
                           school_name: school_name,
                           school_address: school_address,
                           school_type : school_type
@@ -84,9 +75,7 @@ class MyView1 extends PolymerElement {
                     this.$.school_name.value = null;
                     this.$.school_address.value = null;
                     this.$.school_type.value = null;
-                   }
+                  }
  }
-
-
 
 window.customElements.define('my-view1', MyView1);
